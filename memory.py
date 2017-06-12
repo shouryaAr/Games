dd68f43989830a63f47bed468e9082a5ff2494dc
ekaro <marek.pekarovic@gmail.com>
2017-06-12 15:58:22 +0200

    Add files via upload

--- memory.py
+++ memory.py
@@ -12,14 +12,25 @@ def restart():
     exposed=[False]*len(deck)
     print(exposed)
 
+def click(event):
+    x, y = event.x, event.y
+    print('{}, {}'.format(x, y))
 
-MemoryFrame = Frame(master, width=500, height=100, bg="black")
-MemoryFrame.pack()
 
-TurnsLabel = Label(MemoryFrame, text="Turns: 0", font=("Calibri", 32), fg="green", bg="black")
-TurnsLabel.grid(row=1, column = 0, columnspan=1)
+MemoryFrame = Frame(master, width=100, height=100, bg="black")
+MemoryFrame.grid(row=0, column = 0)
 
-RestartButton = Button(MemoryFrame, text="Restart", height=1, width=7, command=restart, font=("Calibri", 32))
+TurnsLabel = Label(MemoryFrame, text="Turns: 0", font=("Calibri", 32), width=10, fg="green", bg="black")
+TurnsLabel.grid(row=1, column = 0)
+
+RestartButton = Button(MemoryFrame, text="Restart", width=10, command=restart, font=("Calibri", 32))
 RestartButton.grid(row=0, column=0)
 
+MemoryCanvas = Canvas(master, width=200, height=150)
+MemoryCanvas.grid(row=0, column = 1, rowspan=2)
+
+MemoryCanvas.create_rectangle(0, 0, 100, 151, fill="blue")
+MemoryCanvas.create_rectangle(100, 0, 200, 151, fill="blue")
+
+master.bind('<Button-1>', click)
 mainloop()
