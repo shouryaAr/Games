1fab09b64ff68f4ce1cbc012054728c7ed17eea2
ekaro <marek.pekarovic@gmail.com>
2017-06-16 09:24:57 +0200

    Add files via upload

--- memory.py
+++ memory.py
@@ -4,6 +4,24 @@ from tkinter import *
 master = Tk()
 master.title('Memory')
 
+class ResizingCanvas(Canvas):
+    def __init__(self, parent, **kwargs):
+        Canvas.__init__(self, parent, **kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self, event):
+        # determine the ratio of old width/height to new width/height
+        wscale = float(event.width)/self.width
+        hscale = float(event.height)/self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas 
+        self.config(width=self.width, height=self.height)
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all", 0 , 0, wscale, hscale)
+
 def new_game():
     global exposed, state, deck, turns
     turns = 0
@@ -20,7 +38,18 @@ def click(event):
     global state, turns, cardone, cardtwo, rect1, rect2, text1, text2
     x, y = event.x, event.y
     print('{}, {}'.format(x, y))
-    card=(x // 100)+(y // 150)*5
+    #card=(x // 100)+(y // 150)*5
+    #print("Card is " + str(card))
+    currentw = MemoryCanvas.winfo_width()
+    currenth = MemoryCanvas.winfo_height()
+    print(str(currentw)+ " " + str(currenth))
+    cardw = currentw / 5
+    cardh = currenth / 4
+    print(str(cardw)+ " " + str(cardh))
+    card=int((x // cardw)+(y // cardh)*5)
+    print("Card is " + str(card))
+    
+    '''
     if exposed[card] == False:
         if state == 0:
             rect1 = MemoryCanvas.create_rectangle(x // 100 * 100, y // 150 * 150, x // 100 * 100 + 100,
@@ -60,9 +89,13 @@ def click(event):
                                              font=("Calibri", 72))
             state = 1
             print(state)
-         
-MemoryFrame = Frame(master, width=100, height=100, bg="black")
-MemoryFrame.grid(row=0, column = 0)
+     '''
+    
+#MemoryFrame = Frame(master, width=100, height=100, bg="black")
+#MemoryFrame.grid(row=0, column = 0)
+
+MemoryFrame = Frame(master)
+MemoryFrame.pack(fill=BOTH, expand=YES)
 
 TurnsLabel = Label(MemoryFrame, text="Turns: 0", font=("Calibri", 32), width=10, fg="green", bg="black")
 TurnsLabel.grid(row=1, column = 0)
@@ -70,8 +103,11 @@ TurnsLabel.grid(row=1, column = 0)
 RestartButton = Button(MemoryFrame, text="Restart", width=10, command=new_game, font=("Calibri", 32))
 RestartButton.grid(row=0, column=0)
 
-MemoryCanvas = Canvas(master, width=500, height=600)
-MemoryCanvas.grid(row=1, column = 0, columnspan=5)
+#MemoryCanvas = Canvas(master, width=500, height=600)
+#MemoryCanvas.grid(row=1, column = 0, columnspan=5)
+
+MemoryCanvas = ResizingCanvas(master, width=500, height=600, bg="red", highlightthickness=0)
+MemoryCanvas.pack(fill=BOTH, expand=YES)
 
 '''
 MemoryCanvas.create_rectangle(0, 0, 101, 151, fill="blue")
@@ -101,5 +137,6 @@ def draw_cards():
 
 new_game()
 draw_cards()
+MemoryCanvas.addtag_all("all")
 master.bind('<Button-1>', click)
 mainloop()
